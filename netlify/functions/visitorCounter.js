fa5ee4f37271418304a04b78dd78893d59c37dfd
Trainee-webdev <trainee@booktrancemedia.com>
2025-04-03 23:48:29 +0800

    Update visitorCounter.js

--- netlify/functions/visitorCounter.js
+++ netlify/functions/visitorCounter.js
@@ -1,13 +1,28 @@
-// functions/visitor-counter.js
-let visitCount = 0;
+const fs = require("fs");
+const path = require("path");
+const filePath = path.join(__dirname, "counter.json");
 
-exports.handler = async function(event, context) {
-  // Increment the visit counter
-  visitCount++;
+exports.handler = async () => {
+  try {
+    let count = 0;
 
-  // Return the count as a JSON response
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ visitCount })
-  };
+    if (fs.existsSync(filePath)) {
+      const data = fs.readFileSync(filePath, "utf8");
+      count = JSON.parse(data).count;
+    }
+
+    count += 1;
+    fs.writeFileSync(filePath, JSON.stringify({ count }), "utf8");
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ count }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Could not update counter" }),
+    };
+  }
 };
+
